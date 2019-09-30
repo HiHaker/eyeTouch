d391c35c6e5cedadbf0f1746751f23fc68597699
Jianlong Hu <hujianlongynu@163.com>
2019-09-30 09:32:05 +0800

    更新了自己的用户

---
+++ eyetouch-seentao-fe/node_modules/yyuap-bpm/lib/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.BpmTaskCopyPanel = exports.BpmCopyContent = exports.BpmLinkAssign = exports.BpmButtonRecall = exports.BpmButtonSubmit = exports.BpmTaskApprovalWrap = exports.BpmWrap = exports.BpmTestCheckTable = exports.BpmTaskApproval = exports.BpmFlowChart = exports.BpmTable = undefined;
+
+var _BpmTable = require('./BpmTable');
+
+var _BpmTable2 = _interopRequireDefault(_BpmTable);
+
+var _BpmFlowChart = require('./BpmFlowChart');
+
+var _BpmFlowChart2 = _interopRequireDefault(_BpmFlowChart);
+
+var _BpmTaskApproval = require('./BpmTaskApproval');
+
+var _BpmTaskApproval2 = _interopRequireDefault(_BpmTaskApproval);
+
+var _BpmTestCheckTable = require('./BpmTestCheckTable');
+
+var _BpmTestCheckTable2 = _interopRequireDefault(_BpmTestCheckTable);
+
+var _BpmWrap = require('./BpmWrap');
+
+var _BpmWrap2 = _interopRequireDefault(_BpmWrap);
+
+var _BpmTaskApprovalWrap = require('./BpmTaskApprovalWrap');
+
+var _BpmTaskApprovalWrap2 = _interopRequireDefault(_BpmTaskApprovalWrap);
+
+var _BpmButtonSubmit = require('./BpmButtonSubmit');
+
+var _BpmButtonSubmit2 = _interopRequireDefault(_BpmButtonSubmit);
+
+var _BpmButtonRecall = require('./BpmButtonRecall');
+
+var _BpmButtonRecall2 = _interopRequireDefault(_BpmButtonRecall);
+
+var _BpmLinkAssign = require('./BpmLinkAssign');
+
+var _BpmLinkAssign2 = _interopRequireDefault(_BpmLinkAssign);
+
+var _BpmCopyContent = require('./BpmCopyContent');
+
+var _BpmCopyContent2 = _interopRequireDefault(_BpmCopyContent);
+
+var _BpmTaskCopyPanel = require('./BpmTaskCopyPanel');
+
+var _BpmTaskCopyPanel2 = _interopRequireDefault(_BpmTaskCopyPanel);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.BpmTable = _BpmTable2.default;
+exports.BpmFlowChart = _BpmFlowChart2.default;
+exports.BpmTaskApproval = _BpmTaskApproval2.default;
+exports.BpmTestCheckTable = _BpmTestCheckTable2.default;
+exports.BpmWrap = _BpmWrap2.default;
+exports.BpmTaskApprovalWrap = _BpmTaskApprovalWrap2.default;
+exports.BpmButtonSubmit = _BpmButtonSubmit2.default;
+exports.BpmButtonRecall = _BpmButtonRecall2.default;
+exports.BpmLinkAssign = _BpmLinkAssign2.default;
+exports.BpmCopyContent = _BpmCopyContent2.default;
+exports.BpmTaskCopyPanel = _BpmTaskCopyPanel2.default;
