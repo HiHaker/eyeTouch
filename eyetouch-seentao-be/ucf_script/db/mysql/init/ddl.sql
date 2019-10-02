ddaa241932a4d695e64d51ea318aaab81cb086e3
Jianlong Hu <hujianlongynu@163.com>
2019-10-02 17:15:59 +0800

    新增了plikes模块

--- eyetouch-seentao-be/ucf_script/db/mysql/init/ddl.sql
+++ eyetouch-seentao-be/ucf_script/db/mysql/init/ddl.sql
@@ -161,6 +161,23 @@ CREATE TABLE `relation` (
         `CREATE_USER` varchar(64) DEFAULT NULL COMMENT '创建人'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
+
+drop table if EXISTS plikes;
+CREATE TABLE `plikes` (
+`ID` VARCHAR(64) NOT NULL COMMENT '主键',
+    PRIMARY KEY (`ID`),
+    `time` VARCHAR(64) DEFAULT NULL COMMENT '发表时间',
+    `pid` VARCHAR(64) DEFAULT NULL COMMENT '帖子id',
+    `uid` VARCHAR(64) DEFAULT NULL COMMENT '用户id',
+        `TENANT_ID` varchar(64) DEFAULT NULL,
+        `DR` int(11) DEFAULT NULL COMMENT '是否删除',
+        `TS` varchar(64) DEFAULT NULL COMMENT '时间戳',
+        `LAST_MODIFIED` varchar(64) DEFAULT NULL COMMENT '修改时间',
+        `LAST_MODIFY_USER` varchar(64) DEFAULT NULL COMMENT '修改人',
+        `CREATE_TIME` varchar(64) DEFAULT NULL COMMENT '创建时间',
+        `CREATE_USER` varchar(64) DEFAULT NULL COMMENT '创建人'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
+
 alter table pimage add constraint FK_Reference_1 foreign key (pid)
       references post (ID) on delete restrict on update restrict;
 
